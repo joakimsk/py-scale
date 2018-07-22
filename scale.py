83a6780570dbdcccad8975c45fbd6b2443166c70
Joakim Skjefstad <skjefstad.joakim@gmail.com>
2018-07-22 21:47:04 +0200

    Converted to threading

--- scale.py
+++ scale.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 
 # Module to interface a Scaleit scale with a Moxa NPORT RS232-ETH converter.
-# Tested on OS X, Python 3, Moxa NPort 5110A, ScaleIT DINI DFWLKI panel with DINI PBQI30 base
+# Tested on OS X, Python 3.7, Moxa NPort 5110A, ScaleIT DINI DFWLKI panel with DINI PBQI30 base
 
 import socket
 import sys
 import time
+from threading import Thread
 
 class Scale:
     def __init__(self, ip, port):
@@ -13,27 +14,22 @@ class Scale:
         self._port = port
         self._socket = socket.socket()
         self._socket.connect((self._ip,int(self._port)))
+
         self._id = 0
         self._status = 'NA'
         self._dweight = 0.0
         self._tweight = 0.0
         self._npcs = 0
         self._unit = 'NA'
-        self._thread_start = time.thread_time()
-        self._last_comms = 0.0
-        try:
-            self.poll(self._socket)
-        except:
-            print("Unexpected error:"+str(sys.exc_info()[0]))
-            raise
 
-    def poll(self, sock):
-        for line in self.readlines(sock):
-            self.scaleline(line)
-            self._last_comms = time.thread_time()
-            print(self._last_comms)
-            #print(self._id, self._status, self._dweight, self._tweight, self._npcs, self._unit)
-        poll()
+        self._worker = Thread(target=self.run, args=())
+        self._worker.daemon = True
+        self._worker.start()
+
+    def run(self):
+        while True:
+            for line in self.readlines(self._socket):
+                self.scaleline(line)
 
     def scaleline(self, line):
         # 6 elements, check page 41 of TECH_MAN_ENG_DFW_v4.pdf for details
@@ -48,27 +44,32 @@ class Scale:
             self._npcs = int(npcs.strip())
             self._unit = str(unit.strip())
         except ValueError as err:
-            print(err)
+            pass
         except:
-            print("Unexpected error:"+str(sys.exc_info()[0]))
-            print(line)
             raise
 
     def readlines(self, sock, recv_buffer=4096, delim='\r\n'):
+        #print('readlines')
         buffer = ''
-        data = True
-        while data:
-            data = sock.recv(recv_buffer).decode()
+        readable = True
+        if readable:
+            data = self._socket.recv(recv_buffer).decode()
             buffer += data
 
             while buffer.find(delim) != -1:
-                line, buffer = buffer.split('\n', 1)
+                line, buffer = buffer.split('\r\n', 1)
                 yield line
         return
+
+    def lastdata(self):
+        return self._id, self._status, self._dweight, self._tweight, self._npcs, self._unit
 
 def main():
     print("running scale module standalone")
     myscale = Scale('192.168.1.4','4001')
+    while True:
+        data = myscale.lastdata()
+        print(data)
 
 if __name__ == '__main__':
     main()
